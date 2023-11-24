4f73105a069d8112555dc543ca25c1357446a77b
Kamil Dabkowski <kamilodabek@gmail.com>
2023-11-25 00:58:09 +0100

    adding photos localy

--- interfejsy-frontend/src/app/graphic-elements/graphic-elements.component.ts
+++ interfejsy-frontend/src/app/graphic-elements/graphic-elements.component.ts
@@ -7,8 +7,8 @@ import { Component } from '@angular/core';
 })
 export class GraphicElementsComponent {
   images = [
-    {name: 'https://cdn.britannica.com/79/232779-050-6B0411D7/German-Shepherd-dog-Alsatian.jpg', caption: 'owczarek'},
-    {name: 'https://www.forbes.com/advisor/wp-content/uploads/2021/04/dogecoin.jpeg-900x510.jpg', caption: 'doge'},
-    {name: 'https://i.kym-cdn.com/entries/icons/facebook/000/047/199/awkwardly_staring_dog.jpg', caption: 'surprised'},
+    {name: '../assets/owczarek.png', caption: 'owczarek'},
+    {name: '../assets/doge.png', caption: 'doge'},
+    {name: '../assets/surprised.png', caption: 'surprised'},
   ];
 }
